3da6d6c0f17251ca6fd39778ad75f66253f53eb3
Josh Palmer <91572133+josh-palmerk@users.noreply.github.com>
2025-10-17 12:33:14 -0600

    init (did not copy ground.cpp or simulator.cpp

---
+++ 230.05.Ponder/thrust.h
@@ -0,0 +1,59 @@
+/***********************************************************************
+ * Header File:
+ *    Thrust : Represents activation of thrusters
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    down, clockwise, and counterclockwise
+ ************************************************************************/
+
+#pragma once
+
+#include "uiInteract.h"  // for Interface
+
+class TestLander;
+class TestThrust;
+
+ /*****************************************************
+  * THRUST
+  * Represents activation of thrusters
+  *****************************************************/
+class Thrust
+{
+   friend TestLander;
+   friend TestThrust;
+   
+public:
+   // Thrust is initially turned off
+   Thrust() : mainEngine(false), clockwise(false), counterClockwise(false) {}
+
+   // Get rotation in radians per second
+   double rotation() const
+   {
+      return (isClock()) ? .1 : -.1;
+   }
+
+   // get main engine thrust in  m / s ^ 2
+   double mainEngineThrust() const
+   {
+      return 45000.0 / 15103.0;
+   }
+
+   // reflect what is firing
+   bool isMain()    const { return mainEngine; }
+   bool isClock()   const { return clockwise; }
+   bool isCounter() const { return counterClockwise; }
+
+   // set the thrusters
+   void set(const Interface * pUI)
+   {
+      mainEngine       = true;
+      clockwise        = pUI->isRight();
+      counterClockwise = pUI->isLeft();
+   }
+
+private:
+   bool mainEngine;
+   bool clockwise;
+   bool counterClockwise;
+};
